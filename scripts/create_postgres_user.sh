69cda99b605475da6d8f56a55a37c52f644e48d8
Alex Hanselka <alex@gitlab.com>
2018-11-07 22:20:54 +0000

    Update create_postgres_user.sh

--- scripts/create_postgres_user.sh
+++ scripts/create_postgres_user.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-psql -h postgres -U postgres postgres <<EOF
+psql -h localhost -U postgres postgres <<EOF
 CREATE USER gitlab;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO gitlab;
 EOF
