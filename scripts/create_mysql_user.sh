64fbfe7900100c33d124c78fb36f035b8a66ccba
Alex Hanselka <alex@gitlab.com>
2018-11-07 22:21:09 +0000

    Update create_mysql_user.sh

--- scripts/create_mysql_user.sh
+++ scripts/create_mysql_user.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mysql --user=root --host=mysql <<EOF
+mysql --user=root --host=localhost <<EOF
 CREATE USER IF NOT EXISTS 'gitlab'@'%';
 GRANT ALL PRIVILEGES ON gitlabhq_test.* TO 'gitlab'@'%';
 FLUSH PRIVILEGES;
