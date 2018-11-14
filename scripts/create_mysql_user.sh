efcffe2d0361fbba002a757ad481d8ce5b58f1db
Robert Speicher <rspeicher@gmail.com>
2018-11-14 14:55:03 +0100

    Use 127.0.0.1 in place of localhost for MySQL
    See https://gitlab.com/gitlab-org/release/framework/issues/59#note_117201248

--- scripts/create_mysql_user.sh
+++ scripts/create_mysql_user.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mysql --user=root --host=localhost <<EOF
-CREATE USER IF NOT EXISTS 'gitlab'@'%';
-GRANT ALL PRIVILEGES ON gitlabhq_test.* TO 'gitlab'@'%';
+mysql --user=root --host=127.0.0.1 <<EOF
+CREATE USER IF NOT EXISTS 'gitlab'@'127.0.0.1';
+GRANT ALL PRIVILEGES ON gitlabhq_test.* TO 'gitlab'@'127.0.0.1';
 FLUSH PRIVILEGES;
 EOF
