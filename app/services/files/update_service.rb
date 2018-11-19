364f80905aeb478b7180887e5241c4c30e749220
Oswaldo Ferreira <oswaldo@gitlab.com>
2018-11-19 13:53:19 -0200

    Make possible to apply patches with the file update API

--- app/services/files/update_service.rb
+++ app/services/files/update_service.rb
@@ -2,7 +2,13 @@
 
 module Files
   class UpdateService < Files::BaseService
+    PatchUpdateError = Class.new(StandardError)
+
     def create_commit!
+      if apply_as_patch?
+        @file_content = apply_content_as_patch
+      end
+
       repository.update_file(current_user, @file_path, @file_content,
                              message: @commit_message,
                              branch_name: @branch_name,
@@ -15,12 +21,39 @@ module Files
 
     private
 
+    def apply_content_as_patch
+      from = @from_line - 1
+      to = @to_line - 1
+      content = branch_blob.data.lines
+      content[from..to] = @file_content
+
+      content.join
+    end
+
     def validate!
       super
 
       if file_has_changed?(@file_path, @last_commit_sha)
         raise FileChangedError, "You are attempting to update a file that has changed since you started editing it."
       end
+
+      if apply_as_patch?
+        raise_patch_error('Invalid range.') unless @from_line.positive? && @from_line.positive? && @to_line >= @from_line
+        raise_patch_error('Blob not found.') unless branch_blob
+        raise_patch_error('Given line surpasses the scope of the file.') if @to_line > branch_blob.lines.size
+      end
+    end
+
+    def apply_as_patch?
+      @from_line && @to_line
+    end
+
+    def raise_patch_error(message)
+      raise PatchUpdateError, message
+    end
+
+    def branch_blob
+      @branch_blob ||= @start_project.repository.blob_at_branch(@branch_name, @file_path)
     end
   end
 end
--- spec/services/files/update_service_spec.rb
+++ spec/services/files/update_service_spec.rb
@@ -10,6 +10,8 @@ describe Files::UpdateService do
   let(:branch_name) { project.default_branch }
   let(:last_commit_sha) { nil }
   let(:commit) { project.repository.commit }
+  let(:from_line) { nil }
+  let(:to_line) { nil }
 
   let(:commit_params) do
     {
@@ -20,7 +22,9 @@ describe Files::UpdateService do
       last_commit_sha: last_commit_sha,
       start_project: project,
       start_branch: project.default_branch,
-      branch_name: branch_name
+      branch_name: branch_name,
+      from_line: from_line,
+      to_line: to_line
     }
   end
 
@@ -62,6 +66,121 @@ describe Files::UpdateService do
         expect(user.commit_email).not_to eq(user.email)
         expect(commit.author_email).to eq(user.commit_email)
         expect(commit.committer_email).to eq(user.commit_email)
+      end
+
+      context 'when should apply content as patch' do
+        context 'valid range' do
+          let(:from_line) { 8 }
+          let(:to_line) { 10 }
+
+          let(:new_contents) do
+            <<-CONTENT
+    if !cmd.is_a?(Array)
+      raise RuntimeError,
+        "System commands must be given as an array of strings"
+    end
+            CONTENT
+          end
+
+          let(:expected_content) do
+            <<-CONTENT.strip_heredoc
+            require 'fileutils'
+            require 'open3'
+
+            module Popen
+              extend self
+
+              def popen(cmd, path=nil)
+                if !cmd.is_a?(Array)
+                  raise RuntimeError,
+                    "System commands must be given as an array of strings"
+                end
+
+                path ||= Dir.pwd
+
+                vars = {
+                  "PWD" => path
+                }
+
+                options = {
+                  chdir: path
+                }
+
+                unless File.directory?(path)
+                  FileUtils.mkdir_p(path)
+                end
+
+                @cmd_output = ""
+                @cmd_status = 0
+
+                Open3.popen3(vars, *cmd, options) do |stdin, stdout, stderr, wait_thr|
+                  @cmd_output << stdout.read
+                  @cmd_output << stderr.read
+                  @cmd_status = wait_thr.value.exitstatus
+                end
+
+                return @cmd_output, @cmd_status
+              end
+            end
+            CONTENT
+          end
+
+          it 'updates the file with the new contents' do
+            subject.execute
+
+            result = project.repository.blob_at_branch(project.default_branch, file_path)
+
+            expect(result.data).to eq(expected_content)
+          end
+        end
+
+        context 'invalid range' do
+          context 'when range surpasses file lines size' do
+            let(:from_line) { 8 }
+            let(:to_line) { 999 }
+
+            it 'raises PatchUpdateError' do
+              expect { subject.execute }
+                .to raise_error(described_class::PatchUpdateError, 'Given line surpasses the scope of the file.')
+            end
+          end
+
+          context 'when lines does not have positive value' do
+            let(:from_line) { -1 }
+            let(:to_line) { 20 }
+
+            it 'raises PatchUpdateError' do
+              expect { subject.execute }
+                .to raise_error(described_class::PatchUpdateError, 'Invalid range.')
+            end
+          end
+
+          context 'when from_line value is higher than to_line' do
+            let(:from_line) { 20 }
+            let(:to_line) { 10 }
+
+            it 'raises PatchUpdateError' do
+              expect { subject.execute }
+                .to raise_error(described_class::PatchUpdateError, 'Invalid range.')
+            end
+          end
+
+          context 'when blob is not found' do
+            before do
+              expect_next_instance_of(Repository) do |repo|
+                allow(repo).to receive(:blob_at_branch) { nil }
+              end
+            end
+
+            let(:from_line) { 3 }
+            let(:to_line) { 3 }
+
+            it 'raises PatchUpdateError' do
+              expect { subject.execute }
+                .to raise_error(described_class::PatchUpdateError, 'Blob not found.')
+            end
+          end
+        end
       end
     end
 
