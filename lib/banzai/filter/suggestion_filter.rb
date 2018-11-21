1ec9f3034fadc036b839d4086c16077ab5eb83fc
Oswaldo Ferreira <oswaldo@gitlab.com>
2018-11-21 20:24:10 -0200

    Comment concerns in the suggestion_filter.rb

--- lib/banzai/filter/suggestion_filter.rb
+++ lib/banzai/filter/suggestion_filter.rb
@@ -6,10 +6,21 @@ module Banzai
       # Class used for tagging elements that should be rendered
       TAG_CLASS = 'js-render-suggestion'.freeze
 
-      # TODO: each code.suggestion should have the data-attributes:
-      # - suggested_lines (with line number and text)
-      # - can_apply
-
+      # Why's of fetching diff-notes here:
+      # 1. We need to provide highlighted content for both
+      #    changing and suggested lines. Though FE is able to fetch
+      #    the highlighted data for the former, it would need to change
+      #    the data for the suggested line in order to highlight.
+      # 2. Backend should be able to say the diff is outdated,
+      #    therefore FE shouldn't be able to allow applying patches.
+      # 3. For non-outdated diff notes with suggestions in
+      #    `Discussion` tab, FE has little context of the current version
+      #    diff.
+      # TODO:
+      # - Add .js-blob-code-addition
+      # - Add .js-blob-code-deletion
+      # - Add a data-outdated-comment
+      #
       def call
         doc.css('pre.code.suggestion > code').each do |el|
           el.add_class(TAG_CLASS)
