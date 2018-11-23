cebc929d4085edd1a629e605184c9bee74d6a95b
Oswaldo Ferreira <oswaldo@gitlab.com>
2018-11-23 17:27:44 -0200

    Improve suggestion creation service to create in batch

--- app/services/suggestions/create_service.rb
+++ app/services/suggestions/create_service.rb
@@ -9,10 +9,33 @@ module Suggestions
     def execute
       suggestions = Banzai::SuggestionsParser.parse(@diff_note.note)
 
-      suggestions.each do |suggestion|
-        @diff_note.suggestions.create(changing: @diff_note.diff_line.text,
-                           suggestion: suggestion)
-      end
+      # For single line suggestion we're only looking forward to
+      # change the line receiving the comment. Though, in
+      # https://gitlab.com/gitlab-org/gitlab-ce/issues/53310
+      # we'll introduce a ```suggestion:L<x>-<y>, so this will
+      # slightly change.
+      comment_index = @diff_note.position.new_line - 1
+
+      rows =
+        suggestions.map do |suggestion|
+          {
+            note_id: @diff_note.id,
+            changing: changing_lines(comment_index, comment_index),
+            suggestion: suggestion
+          }
+        end
+
+      Gitlab::Database.bulk_insert('suggestions', rows)
+    end
+
+    private
+
+    def changing_lines(from_index, to_index)
+      new_blob_lines[from_index..to_index].join("\n")
+    end
+
+    def new_blob_lines
+      @new_blob_lines ||= @diff_note.diff_file.new_blob.lines
     end
   end
 end
---
+++ spec/services/suggestions/create_service_spec.rb
@@ -0,0 +1,72 @@
+require 'spec_helper'
+
+describe Suggestions::CreateService do
+  let(:project_with_repo) { create(:project, :repository) }
+  let(:merge_request) do
+    create(:merge_request, source_project: project_with_repo,
+                           target_project: project_with_repo)
+  end
+
+  let(:position) do
+    Gitlab::Diff::Position.new(old_path: "files/ruby/popen.rb",
+                               new_path: "files/ruby/popen.rb",
+                               old_line: nil,
+                               new_line: 14,
+                               diff_refs: merge_request.diff_refs)
+  end
+
+  let(:diff_note) do
+    create(:diff_note_on_merge_request, project: project_with_repo,
+                                        noteable: merge_request,
+                                        position: position,
+                                        note: markdown)
+  end
+
+  subject { described_class.new(diff_note) }
+
+  describe '#execute' do
+    context 'single line suggestions' do
+      let(:markdown) do
+        <<-MARKDOWN.strip_heredoc
+          ```suggestion
+            foo
+            bar
+          ```
+
+          ```
+            nothing
+          ```
+
+          ```suggestion
+            xpto
+            baz
+          ```
+
+          ```thing
+            this is not a suggestion, it's a thing
+          ```
+        MARKDOWN
+      end
+
+      it 'persists suggestion records' do
+        expect { subject.execute }
+          .to change { diff_note.suggestions.count }
+          .from(0)
+          .to(2)
+      end
+
+      it 'persists original changing lines and suggested lines' do
+        subject.execute
+
+        suggestion_1 = diff_note.suggestions.first
+        suggestion_2 = diff_note.suggestions.last
+
+        expect(suggestion_1).to have_attributes(changing: "    vars = {",
+                                                suggestion: "  foo\n  bar")
+
+        expect(suggestion_2).to have_attributes(changing: "    vars = {",
+                                                suggestion: "  xpto\n  baz")
+      end
+    end
+  end
+end
