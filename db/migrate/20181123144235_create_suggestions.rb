2d38bb322389a0864c6dd9aaf046833c0b41706b
Oswaldo Ferreira <oswaldo@gitlab.com>
2018-11-23 17:28:12 -0200

    Remove timestamp from suggestions

--- db/migrate/20181123144235_create_suggestions.rb
+++ db/migrate/20181123144235_create_suggestions.rb
@@ -4,8 +4,6 @@ class CreateSuggestions < ActiveRecord::Migration[5.0]
       t.text :changing, null: false
       t.text :suggestion, null: false
       t.references :note, foreign_key: true, index: true
-
-      t.timestamps
     end
   end
 end
