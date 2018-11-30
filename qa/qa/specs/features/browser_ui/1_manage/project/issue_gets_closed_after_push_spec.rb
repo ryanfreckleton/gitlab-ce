bc3ec9453715607887f78228d1fe133ba45ac319
Sven Kroell <sven.kroell@gmail.com>
2018-11-30 12:52:30 +0100

    change push file method

--- qa/qa/specs/features/browser_ui/1_manage/project/issue_gets_closed_after_push_spec.rb
+++ qa/qa/specs/features/browser_ui/1_manage/project/issue_gets_closed_after_push_spec.rb
@@ -6,10 +6,9 @@ module QA
       it 'closes the issue after pushing a commit' do
         issue_id = create_issue
         deactivate_auto_devops
-        push_file(issue_id, "firstfile")
-        # It is necessary to push a second file as the issue is not closed with the first file
-        
-        commit_sha = push_file(issue_id, "secondfile")
+        push_file("initial commit", "firstfile")
+        # It is necessary to push a second file as the issue is not closed with the initial commit
+        commit_sha = push_file("Closes ##{issue_id}", "secondfile")
 
         navigate_to_created_issue
         Page::Project::Issue::Show.perform do |page|
@@ -28,13 +27,13 @@ module QA
         Page::Project::Issue::Show.act {issue_id}
       end
 
-      def push_file(issue_id, filename)
+      def push_file(commit_message, filename)
         Page::Project::Menu.act {click_project}
         Page::Project::Show.act {create_new_file!}
         Page::File::Form.perform do |page|
           page.add_name(filename)
           page.add_content('Enable the Web IDE')
-          page.add_commit_message("Closes ##{issue_id}")
+          page.add_commit_message(commit_message)
           page.commit_changes
         end
         Page::File::Created.act {commit_sha}
