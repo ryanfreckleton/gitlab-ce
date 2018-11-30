f13fe1ac41789d6d620c8fa4745ff20dac571103
Sven Kroell <sven.kroell@googlemail.com>
2018-11-30 16:33:13 +0100

    add view element for sha group

--- qa/qa/page/file/created.rb
+++ qa/qa/page/file/created.rb
@@ -2,8 +2,12 @@ module QA
   module Page
     module File
       class Created < Page::Base
+        view 'app/views/projects/commits/_commit.html.haml' do
+          element :commit_sha_group
+        end
+
         def commit_sha
-          page.within('div.commit-sha-group') do
+          within_element(:commit_sha_group) do
             find('div.label').text
           end
         end
