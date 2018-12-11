53212d77f1f7a88963e7ea1bce109c9aa1be1b5b
Peter Leitzen <pleitzen@gitlab.com>
2018-12-11 17:10:27 +0100

    Add and check feature flag `error_tracking`

--- app/controllers/projects/error_tracking_controller.rb
+++ app/controllers/projects/error_tracking_controller.rb
@@ -1,10 +1,18 @@
 # frozen_string_literal: true
 
 class Projects::ErrorTrackingController < Projects::ApplicationController
+  before_action :check_feature_flag!
+
   def list
   end
 
   def index
     render json: []
+  end
+
+  private
+
+  def check_feature_flag!
+    render_404 unless Feature.enabled?(:error_tracking, project)
   end
 end
--- spec/controllers/projects/error_tracking_controller_spec.rb
+++ spec/controllers/projects/error_tracking_controller_spec.rb
@@ -21,6 +21,18 @@ RSpec.describe Projects::ErrorTrackingController, type: :controller do
       expect(response).to render_template(:list)
     end
 
+    context 'with feature flag disabled' do
+      before do
+        stub_feature_flags(error_tracking: false)
+      end
+
+      it 'returns 404' do
+        get :list, project_params
+
+        expect(response).to have_gitlab_http_status(:not_found)
+      end
+    end
+
     context 'with an anonymous user' do
       before do
         sign_out(user)
@@ -40,6 +52,18 @@ RSpec.describe Projects::ErrorTrackingController, type: :controller do
 
       expect(response).to have_gitlab_http_status(:ok)
       expect(json_response).to eq([])
+    end
+
+    context 'with feature flag disabled' do
+      before do
+        stub_feature_flags(error_tracking: false)
+      end
+
+      it 'returns 404' do
+        get :index, project_params
+
+        expect(response).to have_gitlab_http_status(:not_found)
+      end
     end
   end
 
