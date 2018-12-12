f032f15c08be038a6284001d9048db2d907d5541
Peter Leitzen <pleitzen@gitlab.com>
2018-12-12 16:35:29 +0100

    Implement error model

--- app/controllers/projects/error_tracking_controller.rb
+++ app/controllers/projects/error_tracking_controller.rb
@@ -7,54 +7,59 @@ class Projects::ErrorTrackingController < Projects::ApplicationController
   end
 
   def index
-    render json: [
-      {
-          "id": 2,
-          "first_seen": "2018-11-06T21:19:56Z",
-          "last_seen": "2018-11-06T21:19:56Z",
-          "type": "ApiException",
-          "message": "Authentication failed, token expired!",
-          "culprit": "io.sentry.example.ApiRequest in perform",
-          "count": 1,
-          "external_url": "https://sentry.io/the-interstellar-jurisdiction/pump-station/issues/2/",
-          "project": {
-            "id": "2",
-            "name": "Pump Station",
-            "slug": "pump-station"
-          },
-          "short_id": "PUMP-STATION-2",
-          "status": "unresolved",
-          "status_details": {},
-          "title": "ApiException: Authentication failed, token expired!",
-          "type": "error",
-          "user_count": 0
-      },
-      {
-          "id": 3,
-          "first_seen": "2018-10-06T21:19:56Z",
-          "last_seen": "2018-11-06T21:19:56Z",
-          "type": "BigError",
-          "message": "All the things failed.",
-          "culprit": "system.tasks.do.things",
-          "count": 3,
-          "external_url": "https://sentry.io/the-interstellar-jurisdiction/pump-station/issues/3/",
-          "project": {
-            "id": "2",
-            "name": "Pump Station",
-            "slug": "pump-station"
-          },
-          "short_id": "PUMP-STATION-3",
-          "status": "unresolved",
-          "status_details": {},
-          "title": "BigError: All the things failed.",
-          "type": "error",
-          "user_count": 0
-      },
-    ]
-
+    render json: query_errors
   end
 
   private
+
+  def query_errors
+    [
+      {
+        "id": 2,
+        "first_seen": "2018-11-06T21:19:56Z",
+        "last_seen": "2018-11-06T21:19:56Z",
+        "message": "Authentication failed, token expired!",
+        "culprit": "io.sentry.example.ApiRequest in perform",
+        "count": 1,
+        "external_url": "https://sentry.io/the-interstellar-jurisdiction/pump-station/issues/2/",
+        "project": {
+          "id": "2",
+          "name": "Pump Station",
+          "slug": "pump-station"
+        },
+        "short_id": "PUMP-STATION-2",
+        "status": "unresolved",
+        "title": "ApiException: Authentication failed, token expired!",
+        "type": "error",
+        "user_count": 0
+      },
+      {
+        "id": 3,
+        "first_seen": "2018-10-06T21:19:56Z",
+        "last_seen": "2018-11-06T21:19:56Z",
+        "message": "All the things failed.",
+        "culprit": "system.tasks.do.things",
+        "count": 3,
+        "external_url": "https://sentry.io/the-interstellar-jurisdiction/pump-station/issues/3/",
+        "project": {
+          "id": "2",
+          "name": "Pump Station",
+          "slug": "pump-station"
+        },
+        "short_id": "PUMP-STATION-3",
+        "status": "unresolved",
+        "title": "BigError: All the things failed.",
+        "type": "error",
+        "user_count": 0
+      }
+    ].map do |json|
+      project = json.delete(:project)
+      json[:project_id] = project.fetch(:id)
+      json[:project_name] = project.fetch(:name)
+      json[:project_slug] = project.fetch(:slug)
+      ErrorTracking::Error.new(json)
+    end
+  end
 
   def check_feature_flag!
     render_404 unless Feature.enabled?(:error_tracking, project)
