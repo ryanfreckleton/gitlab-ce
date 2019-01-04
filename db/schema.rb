d81a71eab034b338a88be736aaf365d2eb956e8f
Shinya Maeda <shinya@gitlab.com>
2019-01-04 18:33:05 +0900

    Cleanup legacy artifact background migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181219145520) do
+ActiveRecord::Schema.define(version: 20190104182041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
