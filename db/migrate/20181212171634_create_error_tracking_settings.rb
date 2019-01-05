85f563b3a762be0b0b0d8d25bf4bcf236f29c4a1
rpereira2 <rpereira@gitlab.com>
2019-01-05 18:20:00 +0530

    Modify migration to use add_concurrent_foreign_key

--- db/migrate/20181212171634_create_error_tracking_settings.rb
+++ db/migrate/20181212171634_create_error_tracking_settings.rb
@@ -2,10 +2,11 @@
 
 class CreateErrorTrackingSettings < ActiveRecord::Migration[5.0]
   include Gitlab::Database::MigrationHelpers
+  disable_ddl_transaction!
 
   DOWNTIME = false
 
-  def change
+  def up
     create_table :project_error_tracking_settings, id: :int, primary_key: :project_id, default: nil do |t|
       t.boolean :enabled, null: false, default: true
       t.string :api_url, null: false
@@ -13,6 +14,10 @@ class CreateErrorTrackingSettings < ActiveRecord::Migration[5.0]
       t.string :encrypted_token_iv
     end
 
-    add_concurrent_foreign_key :project_error_tracking_settings, :projects, on_delete: :cascade
+    add_concurrent_foreign_key(:project_error_tracking_settings, :projects, column: :project_id)
+  end
+
+  def down
+    drop_table :project_error_tracking_settings
   end
 end
