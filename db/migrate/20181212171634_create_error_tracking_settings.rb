bb7cab70abfd1730c695ec47f8576ab00d3adb2a
rpereira2 <rpereira@gitlab.com>
2019-01-06 14:03:13 +0530

    Create foreign key along with table
    Also fix spec which checks for index on foreign key to also consider
    the primary key index.

--- db/migrate/20181212171634_create_error_tracking_settings.rb
+++ db/migrate/20181212171634_create_error_tracking_settings.rb
@@ -2,22 +2,16 @@
 
 class CreateErrorTrackingSettings < ActiveRecord::Migration[5.0]
   include Gitlab::Database::MigrationHelpers
-  disable_ddl_transaction!
 
   DOWNTIME = false
 
-  def up
+  def change
     create_table :project_error_tracking_settings, id: :int, primary_key: :project_id, default: nil do |t|
       t.boolean :enabled, null: false, default: true
       t.string :api_url, null: false
       t.string :encrypted_token
       t.string :encrypted_token_iv
+      t.foreign_key :projects, column: :project_id
     end
-
-    add_concurrent_foreign_key(:project_error_tracking_settings, :projects, column: :project_id)
-  end
-
-  def down
-    drop_table :project_error_tracking_settings
   end
 end
