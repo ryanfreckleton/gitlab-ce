ff1ea6e2a4851c798159d6c1b1139a57127b870a
rpereira2 <rpereira@gitlab.com>
2019-01-06 14:59:22 +0530

    Use set() to create project object in spec

--- spec/models/error_tracking/project_error_tracking_setting_spec.rb
+++ spec/models/error_tracking/project_error_tracking_setting_spec.rb
@@ -3,14 +3,14 @@
 require 'spec_helper'
 
 describe ErrorTracking::ProjectErrorTrackingSetting do
+  set(:project) { create(:project) }
+
   describe 'Associations' do
     it { is_expected.to belong_to(:project) }
   end
 
   describe 'Validations' do
-    let(:project1) { create(:project) }
-
-    subject { create(:project_error_tracking_setting, project: project1) }
+    subject { create(:project_error_tracking_setting, project: project) }
 
     context 'when api_url is over 255 chars' do
       before do
@@ -25,11 +25,11 @@ describe ErrorTracking::ProjectErrorTrackingSetting do
   end
 
   describe '#api_url' do
-    let(:project) { create(:project) }
     let(:project_error_tracking_setting) { create(:project_error_tracking_setting, project: project) }
 
     it 'sanitizes the api url' do
       project_error_tracking_setting.api_url = "https://replaceme.com/'><script>alert(document.cookie)</script>"
+
       expect(project_error_tracking_setting).to be_valid
       expect(project_error_tracking_setting.api_url).to eq("https://replaceme.com/'>")
     end
