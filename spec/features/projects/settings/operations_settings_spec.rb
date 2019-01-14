3acab3461f3342e0bca38d553038a574ab398bf6
Tristan Read <tread@gitlab.com>
2019-01-14 18:46:34 +0100

    Add operations settings toggle test

--- spec/features/projects/settings/operations_settings_spec.rb
+++ spec/features/projects/settings/operations_settings_spec.rb
@@ -12,6 +12,28 @@ describe 'Projects > Settings > For a forked project', :js do
     project.add_role(user, role)
   end
 
+  describe 'Settings > Operations' do
+    context 'when the error tracking feature flag is enabled' do
+      it 'renders with the error tracking settings expanded by default' do
+        visit project_path(project) + '/settings/operations' # TODO: find out the blessed way for url concatenation
+        wait_for_requests
+
+        expect(page).to have_selector('h4', :text => _("Error Tracking"))
+        expect(page).to have_selector('label', :text => _("Active"))
+      end
+
+      it 'collapses the error tracking settings' do
+        visit project_path(project) + '/settings/operations' # TODO: find out the blessed way for url concatenation
+        wait_for_requests
+
+        find('button.js-settings-toggle').click
+
+        expect(page).to have_selector('h4', :text => _("Error Tracking"))
+        expect(page).to have_no_selector('label', :text => _("Active"))
+      end
+    end
+  end
+
   describe 'Sidebar > Operations' do
     it 'renders the settings link in the sidebar' do
       visit project_path(project)
