15e5aa12bd4fe6d564d6240a2daf0cfa50e595dc
Thong Kuah <tkuah@gitlab.com>
2019-01-18 14:02:02 +1300

    Add service to upgrade cluster applications
    Something will have to call the service, in a later MR

---
+++ app/services/clusters/applications/update_service.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Clusters
+  module Applications
+    class UpdateService < BaseHelmService
+      def execute
+        return if app.updating?
+
+        begin
+          app.make_updating!
+          helm_api.upgrade(upgrade_command)
+
+          #::ClusterWaitForAppUpdateWorker.perform_in(::ClusterWaitForAppUpdateWorker::INTERVAL, app.name, app.id)
+        rescue Kubeclient::HttpError => e
+          log_error(e)
+          app.make_update_errored!("Kubernetes error: #{e.error_code}")
+        rescue StandardError => e
+          log_error(e)
+          app.make_update_errored!("Can't start installation process.")
+        end
+      end
+    end
+  end
+end
