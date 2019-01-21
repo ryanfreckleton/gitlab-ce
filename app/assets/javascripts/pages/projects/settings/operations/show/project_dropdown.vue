7130a10955516d7c21df7b72cdf5306b153e85b5
Tristan Read <tread@gitlab.com>
2019-01-21 16:22:29 +0100

    Add dropdown-menu-toggle to push caret to the far right

--- app/assets/javascripts/pages/projects/settings/operations/show/project_dropdown.vue
+++ app/assets/javascripts/pages/projects/settings/operations/show/project_dropdown.vue
@@ -51,38 +51,49 @@ export default {
 </script>
 
 <template>
-  <!--
-  .dropdown#vue-dropdown-placeholder
-    %button.dropdown-menu-toggle.js-dropdown-toggle{ type: 'button', disabled: true }
-      %span.dropdown-toggle-text
-        = _('Select project')
-        = icon('chevron-down')
-  -->
-  <!-- <div class="dropdown">
-    <button class="dropdown-menu-toggle js-dropdown-toggle" type="button">
-      <span class="dropdown-toggle-text">
-        {{ placeholderText }}
-        <icon :aria-label="__('Error Tracking|Select Project')" name="chevron-down"/>
-      </span>
-    </button>
-  </div>-->
-  <gl-dropdown class="w-100" menu-class="w-100 mw-100" toggle-class="w-100" :text="buttonText">
-    <!-- TODO: make the caret move to the right. could do so like this: -->
-    <!-- <template slot="button-content">
-      <span class="w-100">{{ placeholderText }}</span>
-      <icon
-        :aria-label="__('Error Tracking|Select Project')"
-        name="chevron-down"
-        css-classes="right"
-      />
-    </template>-->
-    <!-- <gl-dropdown-item disabled value class="disabled w-100">Select Project</gl-dropdown-item> -->
-    <gl-dropdown-item
-      v-for="item in list"
-      :key="item.id"
-      :value="item.id"
+  <!-- TODO: Remove this div -->
+  <div>
+    <!-- Following: HTML-only boostrap dropdown menu, for comparison -->
+    <!-- <div class="dropdown">
+      <button class="dropdown-menu-toggle js-dropdown-toggle w-100" type="button">
+        <span class="dropdown-toggle-text">
+          {{ buttonText }}
+          <icon :aria-label="__('Error Tracking|Select Project')" name="chevron-down"/>
+        </span>
+      </button>
+    </div>-->
+    <!--
+      Note: dropdown-menu-toggle class is required to push the toggle to the right of the dropdown.
+      Could fix this in the gitlab-ui component, or in css
+    -->
+    <!--
+      Given how many classes are required here, could add a full-width option to gl-dropdown
+    -->
+    <gl-dropdown
       class="w-100"
-      @click="handleClick"
-    >{{ item.value }}</gl-dropdown-item>
-  </gl-dropdown>
+      menu-class="w-100 mw-100"
+      toggle-class="dropdown-menu-toggle w-100"
+      :text="buttonText"
+    >
+      <!-- TODO: make the caret move to the right. could do so like this: -->
+      <!-- <template slot="button-content">
+        <span class="dropdown-toggle-text w-100">
+          {{ buttonText }}
+          <icon
+            :aria-label="__('Error Tracking|Select Project')"
+            name="chevron-down"
+            css-classes="right"
+          />
+        </span>
+      </template>-->
+      <!-- <gl-dropdown-item disabled value class="disabled w-100">Select Project</gl-dropdown-item> -->
+      <gl-dropdown-item
+        v-for="item in list"
+        :key="item.id"
+        :value="item.id"
+        class="w-100"
+        @click="handleClick"
+      >{{ item.value }}</gl-dropdown-item>
+    </gl-dropdown>
+  </div>
 </template>
