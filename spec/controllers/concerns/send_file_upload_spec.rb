e42ff25412100d2772846177c2448ac6a385226c
Stan Hu <stanhu@gmail.com>
2019-01-23 22:17:31 -0800

    Bump fog-aws to 3.3.0 and associated dependencies
    Fixes issue with AWS V4 signatures not working with Ceph S3:
    https://github.com/fog/fog-aws/issues/462

--- spec/controllers/concerns/send_file_upload_spec.rb
+++ spec/controllers/concerns/send_file_upload_spec.rb
@@ -76,7 +76,7 @@ describe SendFileUpload do
 
         it 'sends a file with a custom type' do
           headers = double
-          expected_headers = %r(response-content-disposition=attachment%3Bfilename%3D%22test.js%22&response-content-type=application/ecmascript)
+          expected_headers = /response-content-disposition=attachment%3Bfilename%3D%22test.js%22&response-content-type=application%2Fecmascript/
           expect(Gitlab::Workhorse).to receive(:send_url).with(expected_headers).and_call_original
           expect(headers).to receive(:store).with(Gitlab::Workhorse::SEND_DATA_HEADER, /^send-url:/)
 
