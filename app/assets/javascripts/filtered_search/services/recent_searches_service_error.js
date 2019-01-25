3a5697a69e2161a14280346805aa9f775552fa17
Mike Greiling <mike@pixelcog.com>
2019-01-25 10:42:06 -0600

    Resolve illegal prototype inheritence

--- app/assets/javascripts/filtered_search/services/recent_searches_service_error.js
+++ app/assets/javascripts/filtered_search/services/recent_searches_service_error.js
@@ -1,11 +1,7 @@
-class RecentSearchesServiceError {
+export default class RecentSearchesServiceError extends Error {
   constructor(message) {
+    super(message);
     this.name = 'RecentSearchesServiceError';
     this.message = message || 'Recent Searches Service is unavailable';
   }
 }
-
-// Can't use `extends` for builtin prototypes and get true inheritance yet
-RecentSearchesServiceError.prototype = Error.prototype;
-
-export default RecentSearchesServiceError;
