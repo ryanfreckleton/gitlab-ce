9568fb6bcde1b7ea7f2f8d56418b76159d2d7de7
Tristan Read <tread@gitlab.com>
2019-01-25 17:29:29 +0100

    Add call to api endpoint

--- app/assets/javascripts/error_tracking_settings/store/actions.js
+++ app/assets/javascripts/error_tracking_settings/store/actions.js
@@ -9,23 +9,31 @@ const transformBackendProject = project => ({
 
 export default {
   loadProjects({ dispatch }, data) {
+    console.log('data', data);
     // TODO: Remove this when backend is hooked up
-    if (true) {
-      return dispatch('receiveLoadProjects', [
-        { id: '10', name: 'Hello' },
-        { id: '11', name: 'Hi there' },
-        { id: '12', name: "I'm a project" },
-      ]);
-    }
 
-    if (true) {
-      return dispatch('receiveLoadProjects', []);
-    }
-
-    const endpoint = data.apiEndpoitUrl;
-    return axios.get(endpoint).then(res => {
+    return axios.post(data.listProjectsEndpoint).then(res => {
+      console.log('status', res.status);
+      console.log('data', res.data);
       dispatch('receiveLoadProjects', res.data.map(transformBackendProject));
     });
+
+    // if (true) {
+    //   return dispatch('receiveLoadProjects', [
+    //     { id: '10', name: 'Hello' },
+    //     { id: '11', name: 'Hi there' },
+    //     { id: '12', name: "I'm a project" },
+    //   ]);
+    // }
+
+    // if (true) {
+    //   return dispatch('receiveLoadProjects', []);
+    // }
+
+    // const endpoint = data.apiEndpoitUrl;
+    // return axios.get(endpoint).then(res => {
+    //   dispatch('receiveLoadProjects', res.data.map(transformBackendProject));
+    // });
   },
   receiveLoadProjects({ commit }, projects) {
     commit(types.RECEIVE_PROJECTS, projects);
--- app/assets/javascripts/pages/projects/settings/operations/show/index.js
+++ app/assets/javascripts/pages/projects/settings/operations/show/index.js
@@ -4,13 +4,20 @@ import store from '~/error_tracking_settings/store';
 import mountProjectDropdown from '~/error_tracking_settings';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const connectEl = document.getElementById('js-error-tracking-connect');
+  const listProjectsEl = document.getElementById('js-error-tracking-list-projects');
+  const dataEl = listProjectsEl;
+
+  console.log('dataset', dataEl.dataset);
+
+  const {
+    dataset: { listProjectsEndpoint },
+  } = dataEl;
 
   mountProjectDropdown();
 
   // Code to test things locally. TODO: Remove when server implementation is ready.
 
-  $(connectEl).on('click', () => {
-    store.dispatch('loadProjects', {});
+  $(listProjectsEl).on('click', () => {
+    store.dispatch('loadProjects', { listProjectsEndpoint });
   });
 });
