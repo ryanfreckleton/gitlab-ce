204fddae90456000243af6fbc3b6fc36604a8128
Natalia Tepluhina <tarya.se@gmail.com>
2019-01-28 18:47:13 +0200

    Add a test for correct selected versions

--- spec/javascripts/diffs/components/compare_versions_dropdown_spec.js
+++ spec/javascripts/diffs/components/compare_versions_dropdown_spec.js
@@ -1,4 +1,3 @@
-// TODO: https://gitlab.com/gitlab-org/gitlab-ce/issues/48034
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import CompareVersionsDropdown from '~/diffs/components/compare_versions_dropdown.vue';
 import diffsMockData from '../mock_data/merge_request_diffs';
@@ -7,24 +6,33 @@ describe('CompareVersionsDropdown', () => {
   let wrapper;
   const targetBranch = { branchName: 'tmp-wine-dev', versionIndex: -1 };
 
-  const factory = (options = {}) => {
+  const factory = (props = {}) => {
     const localVue = createLocalVue();
 
-    wrapper = shallowMount(CompareVersionsDropdown, { localVue, ...options });
+    wrapper = shallowMount(CompareVersionsDropdown, {
+      localVue,
+      propsData: {
+        baseVersionPath: '/gnuwget/wget2/merge_requests/6/diffs?diff_id=37',
+        otherVersions: diffsMockData.slice(1),
+        targetBranch,
+        ...props,
+      },
+    });
   };
 
   afterEach(() => {
     wrapper.destroy();
   });
 
+  it('should render correct selected version', () => {
+    factory();
+    const dropdownToggleElement = wrapper.find('.dropdown-menu-toggle');
+
+    expect(dropdownToggleElement.text()).toBe(targetBranch.branchName);
+  });
+
   it('should render a correct base version link', () => {
-    factory({
-      propsData: {
-        baseVersionPath: '/gnuwget/wget2/merge_requests/6/diffs?diff_id=37',
-        otherVersions: diffsMockData.slice(1),
-        targetBranch,
-      },
-    });
+    factory();
 
     const links = wrapper.findAll('a');
     const lastLink = links.wrappers[links.length - 1];
