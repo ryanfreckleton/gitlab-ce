3660452a2ed9c59496d098b47b14d5a6a020b8c7
NikolayS <gitlab@samokhvalov.com>
2019-01-28 01:09:05 +0000

    4 o 5 steps finished for both events and push_event_payloads

--- db/migrate/20190120234852_int4_pk_stage1_step3of5.rb
+++ db/migrate/20190120234852_int4_pk_stage1_step3of5.rb
@@ -9,20 +9,20 @@ class Int4PkStage1Step3of5 < ActiveRecord::Migration[5.0]
   disable_ddl_transaction!
 
   def up
-
     if Gitlab::Database.postgresql?
-      # Process existing rows (~300M for GitLab.com) in batches
-      # If it fails, this step can be repeated again, the processing will
+      # Process existing rowsin batches.
+      # If it fails, it is OK to re-run this migration. The processing will
       # automatically continue from the position where it failed.
-      # Total time estimate for GitLab.com: ~11.5 hours
+
+      # GitLab.com: ~300M rows, ~11.5 hours of processing
       int4_to_int8_copy("events", "id", "id_new")
 
-      #
+      # GitLab.com: ~210M rows, ~XX hours of processing
       int4_to_int8_copy("push_event_payloads", "event_id", "event_id_new")
     end
   end
 
   def down
-    # nothing
+    # nothing here
   end
 end
