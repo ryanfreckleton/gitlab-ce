628f7520119b437a8b9c8c29602e80b9b14392f0
Tristan Read <tread@gitlab.com>
2019-01-28 14:14:58 +0100

    Update endpoint call to match backend changes

--- app/assets/javascripts/error_tracking_settings/store/actions.js
+++ app/assets/javascripts/error_tracking_settings/store/actions.js
@@ -12,13 +12,32 @@ export default {
     console.log('data', data);
     // TODO: Remove this when backend is hooked up
 
+    // return axios.post(
+    //   `http://localhost:3000/h5bp/html5-boilerplate/list_projects.json`,
+    //   {
+    //     error_tracking_setting: {
+    //       api_host: '<url here>',
+    //       token: '<token here>',
+    //     },
+    //   },
+    //   {
+    //     headers: { Accept: 'application/json, text/plain, */*' },
+    //   },
+    // );
+
     return axios
-      .post(data.listProjectsEndpoint, {
-        error_tracking_setting: {
-          api_host: '<url here>',
-          token: '<token here>',
+      .post(
+        `${data.listProjectsEndpoint}.json`,
+        {
+          error_tracking_setting: {
+            api_host: '<url here>',
+            token: '<token here>',
+          },
         },
-      })
+        {
+          headers: { Accept: 'application/json, text/plain, */*' },
+        },
+      )
       .then(res => {
         console.log('status', res.status);
         console.log('data', res.data);
