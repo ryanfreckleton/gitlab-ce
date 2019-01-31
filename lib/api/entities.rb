c50b849af509016093e28dbda1b721edfe1ba0a3
pderichs <pderichs@gitlab.com>
2019-01-31 16:02:36 +0100

    Fix spec error

--- lib/api/entities.rb
+++ lib/api/entities.rb
@@ -555,7 +555,7 @@ module API
           # Avoids an N+1 query when metadata is included
           options[:issuable_metadata][issue.id].merge_requests_count
         else
-          []
+          issue.merge_requests_count
         end
       end
     end
