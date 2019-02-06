dea015a05306863d3113b9851c487d258d1408f0
Natalia Tepluhina <tarya.se@gmail.com>
2019-02-06 14:57:37 +0200

    Add a spec for selected version name

--- spec/javascripts/diffs/components/compare_versions_dropdown_spec.js
+++ spec/javascripts/diffs/components/compare_versions_dropdown_spec.js
@@ -2,13 +2,15 @@ import { shallowMount, createLocalVue } from '@vue/test-utils';
 import CompareVersionsDropdown from '~/diffs/components/compare_versions_dropdown.vue';
 import diffsMockData from '../mock_data/merge_request_diffs';
 
+const localVue = createLocalVue();
+const targetBranch = { branchName: 'tmp-wine-dev', versionIndex: -1 };
+
 describe('CompareVersionsDropdown', () => {
   let wrapper;
-  const targetBranch = { branchName: 'tmp-wine-dev', versionIndex: -1 };
+
+  const findSelectedVersion = () => wrapper.find('.dropdown-menu-toggle');
 
   const createComponent = (props = {}) => {
-    const localVue = createLocalVue();
-
     wrapper = shallowMount(CompareVersionsDropdown, {
       localVue,
       sync: false,
@@ -20,11 +22,43 @@ describe('CompareVersionsDropdown', () => {
     wrapper.destroy();
   });
 
-  it('should render correct selected version', () => {
-    createComponent();
-    const dropdownToggleElement = wrapper.find('.dropdown-menu-toggle');
+  describe('selected version name', () => {
+    it('shows latest version when latest is selected', () => {
+      createComponent({
+        mergeRequestVersion: {
+          version_path: '123',
+        },
+        startVersion: {
+          version_index: '1',
+        },
+        otherVersions: [
+          {
+            version_index: '1',
+          },
+        ],
+      });
 
-    expect(dropdownToggleElement.text()).toBe(targetBranch.branchName);
+      expect(findSelectedVersion().text()).toBe('latest version');
+    });
+
+    it('shows target branch name for base branch', () => {
+      createComponent({
+        targetBranch,
+      });
+
+      expect(findSelectedVersion().text()).toBe('tmp-wine-dev');
+    });
+
+    it('shows correct version for non-base and non-latest branches', () => {
+      createComponent({
+        startVersion: {
+          version_index: '1',
+        },
+        targetBranch,
+      });
+
+      expect(findSelectedVersion().text()).toBe('version 1');
+    });
   });
 
   it('should render a correct base version link', () => {
